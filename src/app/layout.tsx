359d7bf37f1d2a3679d0585d0785a4317903dd79
muslimbek-sotvoldiyev <muslimbeksotvoldiyev581@gmail.com>
2026-05-01 14:04:32 +0500

    add wards

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,6 +18,7 @@ export const metadata: Metadata = {
   },
 };
 
+
 export default async function RootLayout({ children }: { children: React.ReactNode }) {
   const locale = await getLocale();
   const messages = await getMessages();
