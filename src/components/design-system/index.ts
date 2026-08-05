91b99ba19dd9db925c2371b34eb96e8c69efc4d1
Abdurazzoq Abdusalomov <rayyan.dev@mail.ru>
2026-08-05 22:45:32 +0500

    feat: add closeOnBackdrop prop to Modal and Sheet components

--- src/components/design-system/index.ts
+++ src/components/design-system/index.ts
@@ -8,8 +8,8 @@ export { Button, type ButtonProps } from "./Button";
 export { Card, CardBody, CardFooter, CardHeader, type CardBodyProps, type CardFooterProps, type CardHeaderProps, type CardProps } from "./Card";
 export { Input, Select, Textarea, type InputProps, type SelectProps, type TextareaProps } from "./Input";
 
-// Modal & Dialog
-export { Drawer, Modal, type DrawerProps, type ModalProps } from "./Modal";
+// Modal
+export { Modal, type ModalProps } from "./Modal";
 
 // Layout Components
 export { Container, GridLayout, PageLayout, Stack, type ContainerProps, type GridLayoutProps, type PageLayoutProps, type StackProps } from "./layouts/Container";
